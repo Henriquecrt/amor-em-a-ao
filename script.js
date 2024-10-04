92188397393fad52a14add20c156f4910fbd6650
Henriquecrt <henriquebpmteste@gmail.com>
2024-10-04 00:38:28 -0300

    alguns ajustes pro tamanho do celular

--- script.js
+++ script.js
@@ -1,48 +1,48 @@
 // Função para a galeria de projetos
 function handleProjectGallery() {
     const projectLinks = document.querySelectorAll(".project-link"); // Seleciona todos os botões
-  
+
     projectLinks.forEach((link) => {
         link.addEventListener("click", (event) => {
             event.preventDefault();
             const projectId = link.dataset.projectId; // Obtém o ID do projeto
-  
+
             // Encontre o elemento project-extra-images
             const targetImages = document.querySelector(`#extra-images-${projectId}`); // Seleciona a div correta
-  
+
             // Mostra ou esconde as fotos extras
             targetImages.classList.toggle("hidden");
-  
+
             // Altera o texto do link para "Ver Menos" ou "Ver Mais"
             if (targetImages.classList.contains("hidden")) {
                 link.textContent = "Ver Mais";
             } else {
                 link.textContent = "Ver Menos";
             }
-  
+
             event.stopPropagation(); // Impede a propagação do evento
         });
     });
-  }
-  
-  // Chame a função quando a página carregar
-  window.addEventListener("load", () => {
+}
+
+// Chame a função quando a página carregar
+window.addEventListener("load", () => {
     handleProjectGallery();
-  });
-  
-  // Função para rolagem suave
-  function smoothScroll(targetId) {
+});
+
+// Função para rolagem suave
+function smoothScroll(targetId) {
     const target = document.querySelector(targetId);
     if (target) {
         target.scrollIntoView({ behavior: "smooth" });
     }
-  }
-  // Função para controlar o menu responsivo
-  function toggleMenu() {
-      const menu = document.querySelector("nav ul");
-      menu.classList.toggle("show");
-  }
-  
-  // Adiciona um evento de clique ao botão do menu
-  const menuToggle = document.querySelector(".menu-toggle");
-  menuToggle.addEventListener("click", toggleMenu);
+}
+// Função para controlar o menu responsivo
+function toggleMenu() {
+    const menu = document.querySelector("nav ul");
+    menu.classList.toggle("show");
+}
+
+// Adiciona um evento de clique ao botão do menu
+const menuToggle = document.querySelector(".menu-toggle");
+menuToggle.addEventListener("click", toggleMenu);
